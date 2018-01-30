ec00d109b5f335d7f8b7d956c10d7a891adefa12
Jesus Diaz-Barriga <jagustin2791@gmail.com>
2018-01-29 20:14:40 -0800

    first commit

---
+++ app/models.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+""" EVENT Website. Users can create a new event, invite people, customize page, send notification, see list of people going, view event that you have been invited on """
+class Emails(models.Model):
+    email = models.CharField(max_length=150)
+    name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
+class NewEvent(models.Model):
+    event_name = models.CharField(max_length=150)
+    date = models.DateField(auto_now=False)
+    date_end = models.DateField(auto_now=False,
+                                blank=True,
+                                null=True)
+    time_start = models.TimeField(auto_now=False)
+    end_time = models.TimeField(auto_now=False)
+    host = models.ForeignKey(User)
+    location = models.CharField(max_length=150)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=50)
+    zip_code = models.IntegerField()
+    guests = models.ManyToManyField(Emails)
+    description = models.TextField()
+    template = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.event_name
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.IntegerField(max_length=10,
+                                       blank=True,
+                                       null=True)
+    email = models.ForeignKey(Emails)
+
+    def __unicode__(self):
+        return self.name
